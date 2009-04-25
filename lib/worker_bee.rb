0b34f171e80966264a48aa82818ea339e4f778d7
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-04-25 15:47:30 -0700

    basic requirements for the application appear to be complete, minus the run method, which should be pretty easy to just put in.

--- lib/worker_bee.rb
+++ lib/worker_bee.rb
@@ -1,28 +1,50 @@
 #!/usr/bin/env ruby -w
 
-class WorkerBee
-  VERSION = '1.0.0'
+require 'pp'
 
-  class << self; attr_accessor :tasks; end
+module Kernel
+  alias :old_puts :puts
+  def puts(string)
+    string
+  end
+end
+
+class WorkerBee
+  VERSION = '0.0.2'
+
+  class << self; attr_accessor :tasks, :already_ran; end
   
   def initialize
   end
   
   def self.recipe(&block)
     @tasks = []
+    @already_ran = []
     instance_eval(&block)
   end
   
   def self.work(*symbols, &block)
     symbol = symbols.shift
     tasks << symbol
-    foo = block.class || nil
-
-    self.class_eval %{
-      def self.#{symbol}(*deps)
-        puts "Running #{symbol}"
-        completed_msg = ""
+    completed_msg = (yield if block_given?) || ""
+    dep_calls = ""
+    symbols.each {|sym|
+      dep_calls += %{msg = self.#{sym}(msg, indent+2)\n}
+    }
+    new_def = %{
+      def self.#{symbol}(msg=[], indent=0)
+        indenting = ""
+        indent.times do indenting = "\#\{indenting\} "; end
+        if already_ran.include?("#{symbol}")
+          msg << "\#\{indenting\}not running #{symbol} - already met dependency"
+        else
+          msg << "\#\{indenting\}Running #{symbol}"
+          #{dep_calls}
+          already_ran << "#{symbol}" unless already_ran.include?("#{symbol}")
+          msg << "#{completed_msg}"
+        end
       end
     }
+    self.class_eval new_def
   end
 end
--- test/test_worker_bee.rb
+++ test/test_worker_bee.rb
@@ -23,6 +23,10 @@ class TestWorkerBee < Test::Unit::TestCase
     assert WorkerBee.tasks.empty?
   end
   
+  def test_has_work
+    assert WorkerBee.respond_to?(:work)
+  end
+    
   def test_recipe_with_one_work_has_one_task
     WorkerBee.recipe do
       work :test
@@ -31,15 +35,40 @@ class TestWorkerBee < Test::Unit::TestCase
     assert_equal 1, WorkerBee.tasks.size
     assert_equal "test", WorkerBee.tasks.first.to_s
   end
-  
-  def test_has_work
-    assert WorkerBee.respond_to?(:work)
-  end
-  
+
   def test_recipe_registers_new_work
     WorkerBee.recipe do
       work :test
     end
     assert WorkerBee.respond_to?(:test)
+  end
+  
+  def test_recipe_registers_new_work_and_it_lets_us_know_its_running
+    WorkerBee.recipe do
+      work :test do
+        puts "** testing!"
+      end
+    end
+    
+    assert_equal ["Running test","** testing!"], WorkerBee.test
+  end
+  
+  def test_recipe_with_two_works_one_dependent_on_the_other
+    WorkerBee.recipe do
+      work :first_test, :second_test do
+        puts "** testing last!"
+      end
+      
+      work :second_test do
+        puts "** testing first!"
+      end
+    end
+    expected = ["Running first_test", "  Running second_test", "** testing first!", "** testing last!"]
+    assert_equal expected, WorkerBee.first_test
+  end
+  
+  def test_recipe_with_work_dependent_twice_only_runs_once
+    actual = WorkerBee.sammich
+    assert actual.include?("    not running clean - already met dependency")
   end
 end
